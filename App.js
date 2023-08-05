94393537805badf90a89d63c64e445116298d410
onkarnawale1998 <onkarnawale1998@gmail.com>
2023-08-05 23:54:40 +0530

    removed comments

--- App.js
+++ App.js
@@ -1,32 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//This is javascript code
-// function attachEventListner(){
-// let count = 0;
-// document.getElementById("clickMe").addEventListener("click" , function xyz() {
-//     console.log("Button is Clicked" , ++count)   
-// })
-// }
-//  attachEventListner()
-
-// const heading =document.createElement("h1")
-// heading.innerHTML = "Namaste Everyone"
-// document.getElementById("root").appendChild(heading);
-
-//This is react code without writing JSX with createElement and createRoot , render
-
-// const heading1 = React.createElement("h1", {id: "title"}, "Namaste React from h1");
-
-
-// const heading2 = React.createElement("h2", {id: "sub_title"}, "Namaste React from h2");
-
-// const heading3 = React.createElement("h2", {id: "sub_titles"}, "Namaste React from h2");
-
-// const container = React.createElement("div", {id:"container"}, [heading1,heading2,heading3])
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//   root.render(container)
 
 const Logo =  () => (
   <a href="/">
@@ -1774,9 +1747,7 @@ const Body = () => {
 
 const Footer = () => {
 
-  return(   
-// Since ReasonReact 0.7.0 you can use
-    
+  return(     
     <em>Copyright Onkar Nawale (feel free to copy)</em>
   )
 }
