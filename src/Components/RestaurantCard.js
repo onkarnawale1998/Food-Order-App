a5f90fb5fd31ec10ffe973f5a741a832a13ad771
onkarnawale1998 <onkarnawale1998@gmail.com>
2023-08-06 18:37:24 +0530

    move image hardcoding to constants

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,8 +1,9 @@
+import {IMG_CDN_URL} from "../../config"
 const RestaurantCard = ( {cloudinaryImageId, name , locality ,areaName,avgRating,cuisines,totalRatingsString,costForTwo, sla:{lastMileTravelString , slaString} }) => {
   
     return(
       <div className="card"> 
-        <img src= {"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+ cloudinaryImageId }  alt="Restaurant image"  />
+        <img src= {IMG_CDN_URL + cloudinaryImageId }  alt="Restaurant image"  />
         <p>{name}</p>
         <h4>{locality+ ", " +areaName}</h4>
         <h5>{avgRating} Stars ({totalRatingsString}ratings), {costForTwo}</h5>
