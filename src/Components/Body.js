0990f5afa6e298c81601179f2daffe48b1ff2598
onkarnawale1998 <onkarnawale1998@gmail.com>
2023-08-07 22:28:46 +0530

    Search function work step 1

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,49 @@
 import RestaurantCard from "./RestaurantCard";
 import {restaurantList} from "../../src/config"
+import { useState } from "react";
 
 const Body = () => {
+
+  const[restaurants, setRestaurants]=useState(restaurantList)
+  const [searchTxt, setSearchTxt] = useState("")
+  // const [searchClicked, setSearchClicked] = useState("")
+  const filterData =  (searchTxt,restaurants) => {
+    return restaurants.filter((restaurant) =>{
+      return restaurant.info.name.includes(searchTxt);
+  })
+  }
     return(
+    <>
+      <input
+      type="text"
+      placeholder="Search"
+      className="search-input"
+      value={searchTxt}
+      onChange={(e) => {
+       setSearchTxt(e.target.value) ;       
+      }}
+      
+    /> 
+     <button 
+      className="search-btn" 
+    //  onClick= {()=>{
+    //   searchClicked === "false" ? setSearchClicked("true") : setSearchClicked("false");
+    //  }}
+      onClick={()=>{
+        const data = filterData(searchTxt,restaurants);
+        {console.log(data)}
+        setRestaurants(data);
+    }}
+
+    //oncick it shoud seach data from restaurant list and filter that cards only
+     >Search
+     </button>
     <div className="restaurant-list">
-      {restaurantList.map((restaurant) => {
+      {restaurants.map((restaurant) => {
         return <RestaurantCard {...restaurant.info} key= {restaurant.info.id} />
       })}
       </div>
+    </>
     )
   }
 export default Body;  
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,31 +1,28 @@
-const Logo =  () => (
-    <a href="/">
-    <img 
-    className='logo'
-    src="https://i.pinimg.com/474x/e6/17/f1/e617f1bfb9af4d9cf132cd3dec0da072.jpg" 
-    alt="Food Villa" 
+const Logo = () => (
+  <a href="/">
+    <img
+      className="logo"
+      src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTFzY7CNCsygf7Hi2pwSekFKEpncFa6wNn0AwDn6NHOSd_6NskU"
+      alt="Food Villa"
     />
-    </a>
-  )
-  
-  const Header = () => {
-    return( 
-      <div className="header">
+  </a>
+);
+
+const Header = () => {
+  return (
+    <div className="header">
       <Logo />
-        <div className="nav-items">
-          <ul>
-            <li>
-            <input placeholder='Search'></input>  
-            </li>
-            <a href='/'>
-              <li>Home</li>
-            </a>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Cart</li>         
-          </ul>
-        </div>
+      <div className="nav-items">
+        <ul>
+          <a href="/">
+            <li>Home</li>
+          </a>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
       </div>
-    )
-  }
-  export default Header;
+    </div>
+  );
+};
+export default Header;
